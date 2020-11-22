7d9dcc7de92e326b31ace70b57424a85cc665fe9
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-22 14:15:32 -0400

    Exibindo variáveis, manipulando rotas, bind's

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 //Estou dizendo para o express usar o EJS como View Engine
 app.set('view engine','ejs');
 
-app.get("/", (req, res) => {
-  res.render("principal/perfil");
+app.get("/:nome/:lang", (req, res) => {
+  var nome = req.params.nome;
+  var lang = req.params.lang;
+  res.render("index", {
+    nome: nome,
+    lang: lang,
+    empresa: "Guia do Programador",
+    inscritos: 8000
+  });
 });
 
 app.listen(8081, () => {console.log("Aplicação rodando!")});
