11f7246dded58a6430e6f61fc7fcfb15700282c0
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-22 15:02:58 -0400

    Estrutura de repetição com ejs

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
   var exibirMsg = true;
+
+  var produtos = [
+    {nome: "Extreme Repair", preco: 120.00},
+    {nome: "Extreme Long", preco: 110.00},
+    {nome: "Extreme Naturals", preco: 55.80},
+    {nome: "Extreme Skins", preco: 66.40},
+    {nome: "OX", preco: 33.43},
+    {nome: "Pó Descolorante", preco: 28.30}
+  ];
+
   res.render("index", {
     nome: nome,
     lang: lang,
     empresa: "Guia do Programador",
     inscritos: 8000,
-    msg: exibirMsg
+    msg: exibirMsg,
+    produtos: produtos
   });
 });
 
