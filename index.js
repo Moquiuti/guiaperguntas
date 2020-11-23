2f4b918fe3349e97b46492911dea5800eaed4f73
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-23 13:40:49 -0400

    Enviando dados do formulário para o backend com bodyparser

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Estou dizendo para o express usar o EJS como View Engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
-
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("Formulário Recebido." + titulo + descricao);
 });
 
 
