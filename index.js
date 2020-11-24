647a7b6e4fe4c43b9081f405efc9c9c8e05350e9
Leandro dos Santos Moquiuti Ferreira <leandro.moquiuti@gmail.com>
2020-11-23 23:04:06 -0400

    Finalizado estilização de formulário de respostas

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 //Rotas
 app.get("/", (req, res) => {
-  Pergunta.findAll({raw: true}).then(perguntas => {
+  Pergunta.findAll({raw: true, order: [
+    ['id','DESC']
+  ]}).then(perguntas => {
     res.render("index", {
         perguntas: perguntas
     });
@@ -41,5 +43,19 @@ app.post("/salvarpergunta", (req, res) => {
   });
 });
 
+app.get("/pergunta/:id", (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: {id: id}
+  }).then(pergunta => {
+    if(pergunta != undefined){
+      res.render("pergunta", {
+        pergunta: pergunta
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 app.listen(8081, () => {console.log("Aplicação rodando!")});
